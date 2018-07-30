4afcf6f23076587a4d8ae4b606a9024cb73dce8e
onurcankaya <onur.eren.cankaya@gmail.com>
2018-07-30 14:22:20 +0200

    implemented error messages for callback errors

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const request = require('request')
+const yargs = require('yargs')
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'address to watch the weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h').argv
+
+const address = encodeURIComponent(argv.address)
 
 request(
   {
-    url:
-      'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json: true,
   },
   (error, response, body) => {
-    console.log(body)
+    if (error) {
+      console.log('Unable to connect to Google servers')
+    } else if (body.status === 'ZERO_RESULTS') {
+      console.log('Unable to find that address')
+    } else if (body.status === 'OK') {
+      console.log(`Address: ${body.results[0].formatted_address}`)
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
+    }
   }
 )
