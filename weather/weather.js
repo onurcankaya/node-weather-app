04f0f655cfdf256bbb694e11cba833e9b515f371
onurcankaya <onur.eren.cankaya@gmail.com>
2018-08-01 12:51:08 +0200

    implemented getWeather function

---
+++ weather/weather.js
@@ -0,0 +1,33 @@
+const request = require('request')
+const apiKey = require('../api-key')
+
+const convertFahrenheitToCelcius = (f) => {
+  return ((f - 32) * 5) / 9
+}
+
+const getWeather = (lat, long, callback) => {
+  request(
+    {
+      url: `https://api.darksky.net/forecast/${apiKey.apiKey()}/${lat},${long}`,
+      json: true,
+    },
+    (error, body, response) => {
+      if (error) {
+        callback('Unable to connect to Darksky.net server.')
+      } else if (response.statusCode === 404) {
+        callback('Unable to fetch weather.')
+      }
+      callback(
+        `It is ${convertFahrenheitToCelcius(
+          response.currently.temperature
+        )} celcius, but it feels like ${convertFahrenheitToCelcius(
+          response.currently.apparentTemperature
+        )}`
+      )
+    }
+  )
+}
+
+module.exports = {
+  getWeather,
+}
